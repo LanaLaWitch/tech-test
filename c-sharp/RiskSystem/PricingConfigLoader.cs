c587c2ea6806902690aa58d46c0a31e93ad8253b
Callum Wells <callumscottwells@live.co.uk>
2025-12-17 00:42:56 +0000

    Implementing PricingConfigLoader (Exercise 3)

--- c-sharp/RiskSystem/PricingConfigLoader.cs
+++ c-sharp/RiskSystem/PricingConfigLoader.cs
@@ -1,4 +1,8 @@
-﻿namespace HmxLabs.TechTest.RiskSystem
+﻿using System.Reflection;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace HmxLabs.TechTest.RiskSystem
 {
     public class PricingConfigLoader
     {
@@ -6,7 +10,32 @@
 
         public PricingEngineConfig LoadConfig()
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(ConfigFile))
+                throw new ArgumentException("ConfigFile property is not set.");
+
+            PricingEngineConfig configList = new PricingEngineConfig();
+
+            // Get the XmlRoot attribute. This means we only need to change XmlRoot attribute value to control deserialization
+            var targetXmlRoot = typeof(PricingEngineConfigItem).GetCustomAttribute<XmlRootAttribute>();
+
+            var deserializer = new XmlSerializer(typeof(PricingEngineConfigItem));
+            using (var reader = XmlReader.Create(ConfigFile))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement(targetXmlRoot?.ElementName ?? ""))
+                    {
+                        var config = (PricingEngineConfigItem)deserializer.Deserialize(reader);
+
+                        if (null == config)
+                            throw new InvalidOperationException("Failed to deserialize pricing engine configuration.");
+
+                        configList.Add(config);
+                    }
+                }
+            }
+
+            return configList;
         }
     }
 }
--- c-sharp/RiskSystem/PricingEngineConfigItem.cs
+++ c-sharp/RiskSystem/PricingEngineConfigItem.cs
@@ -1,11 +1,17 @@
-﻿namespace HmxLabs.TechTest.RiskSystem
+﻿using System.Xml.Serialization;
+
+namespace HmxLabs.TechTest.RiskSystem
 {
+    [XmlRoot("Engine")]
     public class PricingEngineConfigItem
     {
+        [XmlAttribute("tradeType")]
         public string? TradeType { get; set; }
 
+        [XmlAttribute("assembly")]
         public string? Assembly { get; set; }
 
+        [XmlAttribute("pricingEngine")]
         public string? TypeName { get; set; }
     }
 }
