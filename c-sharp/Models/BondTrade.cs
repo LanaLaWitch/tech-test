ffc3f79a7d4da965d7629071bd0c1f4e217b26c6
Lana Wells <lana_wells23@hotmail.com>
2025-12-18 14:01:41 +0000

    Reverting accidental commits from exercise 5

--- c-sharp/Models/BondTrade.cs
+++ c-sharp/Models/BondTrade.cs
@@ -3,28 +3,10 @@
     public class BondTrade : BaseTrade
     {
         public BondTrade(string tradeId_) : base(tradeId_) { }
-    
-        public enum BondTradeTypes
-        {
-            GovBond,
-            CorpBond
-        }
 
         public const string GovBondTradeType = "GovBond";
         public const string CorpBondTradeType = "CorpBond";
 
-        public BondTradeTypes BondTradeType { get; set; }
-
-        public override string TradeType { 
-            get 
-            {
-                return BondTradeType switch
-                {
-                    BondTradeTypes.GovBond => GovBondTradeType,
-                    BondTradeTypes.CorpBond => CorpBondTradeType,
-                    _ => throw new ArgumentException("Unknown bond trade type", BondTradeType.ToString())
-                };
-            } 
-        }
+        public override string TradeType { get { return GovBondTradeType; } }
     }
 }
